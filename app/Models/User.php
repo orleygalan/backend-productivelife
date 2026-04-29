c34c00fdd4948730fa7044b3fe86e236f7f017b3
Orley-Galan <orleigalan@gmail.com>
2026-04-28 19:23:31 -0500

    fix: agregar HasApiTokens de Sanctum al modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasUuids;
     /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasApiTokens;
     use HasFactory, Notifiable;
 
     /**
